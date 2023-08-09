f7f75db1d7f8f4d8551a1ba9f2d55c9a52748d2e
abdo ali <abdoali796@gamil.com>
2023-08-10 01:23:41 +0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -99,3 +100,107 @@ implementation("androidx.compose.material:material-icons-extended:1.4.3")
 kapt {
     correctErrorTypes = true
 }
+||||||| (empty tree)
+=======
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.abdoali.mymidia3"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.abdoali.mymidia3"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt") ,
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+//        allWarningsAsErrors = false
+//        freeCompilerArgs += [
+//            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+//        ]
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+}
+
+dependencies {
+    implementation(project(":playservice"))
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.compose.material3:material3:1.1.1")
+    implementation ("androidx.compose.material3:material3-window-size-class:1.1.1")
+    implementation(project(mapOf("path" to ":playservice")))
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    val media3_version = "1.1.0"
+implementation("androidx.compose.material:material-icons-extended:1.4.3")
+
+    implementation("androidx.media3:media3-common:$media3_version")
+    implementation("com.google.accompanist:accompanist-permissions:0.31.5-beta")
+
+    implementation("com.google.dagger:hilt-android:2.44")
+//    kapt ("com.google.dagger:hilt-compiler:2.45")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("io.coil-kt:coil-compose:2.3.0")
+
+    implementation ("com.github.skydoves:cloudy:0.1.2")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+}
+kapt {
+    correctErrorTypes = true
+}
+>>>>>>> 25daa1b (Initial commit)
